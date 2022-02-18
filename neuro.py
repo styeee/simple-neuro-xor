f3f1da31a2dd62aa67b4d3b593532ccfac87976e
styeee <91516623+styeee@users.noreply.github.com>
2022-02-18 15:45:29 +0700

    Add files via upload

---
+++ neuro.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+
+SVC_model=svm.SVC()
+
+x=np.array([
+    [0,0],
+    [1,1],
+    [0,1],
+    [1,0],
+])
+y=np.array([1,1,0,0])
+
+for i in range(10000):
+    SVC_model.fit(x,y)
+
+v=np.array([[0,0]])
+
+print(SVC_model.predict(v))
